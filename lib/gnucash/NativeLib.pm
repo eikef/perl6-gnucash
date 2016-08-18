771a589553b80919e186d412d815e9cdc9bd9552
Eike Frost <ei@kefro.st>
2016-08-19 00:33:47 +0200

    use correct libraries to resolve symbols

--- lib/gnucash/NativeLib.pm
+++ lib/gnucash/NativeLib.pm
@@ -9,13 +9,19 @@ use NativeCall;
 sub gncmod-engine {
     $*VM.platform-library-name('gncmod-engine'.IO).Str;
 }
+sub gnc-qof {
+    $*VM.platform-library-name('gnc-qof'.IO).Str;
+}
+sub gnc-module {
+    $*VM.platform-library-name('gnc-module'.IO).Str;
+}
 
 
-sub qof_log_init() is native(&gncmod-engine) is export { * }
+sub qof_log_init() is native(&gnc-qof) is export { * }
 
-sub qof_init() is native(&gncmod-engine) is export { * }
+sub qof_init() is native(&gnc-qof) is export { * }
 
-sub gnc_module_system_init() is native(&gncmod-engine) is export { * }
+sub gnc_module_system_init() is native(&gnc-module) is export { * }
 
 sub gnc_engine_init(int64 $argc, CArray[Str] $argv -->OpaquePointer) is native(&gncmod-engine) is export { * }
 
@@ -28,40 +34,40 @@ class QofSession is repr('CPointer') is export {
     method begin(Str $book_id, int64 $ignore_lock = 0, int64 $create = 0, int64 $force = 0) { self.qof_session_begin($book_id, $ignore_lock, $create, $force) }
 
     # Native methods
-    method qof_session_new(-->QofSession) is native(&gncmod-engine) { * }
+    method qof_session_new(-->QofSession) is native(&gnc-qof) { * }
 
-    method qof_session_begin(Str $book_id, int64 $ignore_lock, int64 $create, int64 $force) is native(&gncmod-engine) { * }
+    method qof_session_begin(Str $book_id, int64 $ignore_lock, int64 $create, int64 $force) is native(&gnc-qof) { * }
 
-    method qof_session_swap_data(QofSession $session2) is native(&gncmod-engine) { * }
+    method qof_session_swap_data(QofSession $session2) is native(&gnc-qof) { * }
 
-    method qof_session_load(&percentage_func (Str $message, num64 $percent)) is native(&gncmod-engine) { * }
+    method qof_session_load(&percentage_func (Str $message, num64 $percent)) is native(&gnc-qof) { * }
 
     # TODO: replace Opaquepointer with QofBackendError
-    method qof_session_get_error(-->OpaquePointer) is native(&gncmod-engine) { * }
-    method qof_session_get_error_message(-->Str) is native(&gncmod-engine) { * }
-    method qof_session_pop_error(-->OpaquePointer) is native(&gncmod-engine) { * }
+    method qof_session_get_error(-->OpaquePointer) is native(&gnc-qof) { * }
+    method qof_session_get_error_message(-->Str) is native(&gnc-qof) { * }
+    method qof_session_pop_error(-->OpaquePointer) is native(&gnc-qof) { * }
 
     # TODO: replace OpaquePointer with QofBook
-    method qof_session_get_book(-->OpaquePointer) is native(&gncmod-engine) { * }
+    method qof_session_get_book(-->OpaquePointer) is native(&gnc-qof) { * }
 
-    method qof_session_get_file_path(-->Str) is native(&gncmod-engine) { * }
-    method qof_session_get_url(-->Str) is native(&gncmod-engine) { * }
+    method qof_session_get_file_path(-->Str) is native(&gnc-qof) { * }
+    method qof_session_get_url(-->Str) is native(&gnc-qof) { * }
 
-    method qof_session_save_in_progress(-->int64) is native(&gncmod-engine) { * }
+    method qof_session_save_in_progress(-->int64) is native(&gnc-qof) { * }
 
-    method qof_session_save(&percentage_func (Str $message, num64 $percent)) is native(&gncmod-engine) { * }
-    method qof_session_safe_save(&percentage_func (Str $message, num64 $percent)) is native(&gncmod-engine) { * }
+    method qof_session_save(&percentage_func (Str $message, num64 $percent)) is native(&gnc-qof) { * }
+    method qof_session_safe_save(&percentage_func (Str $message, num64 $percent)) is native(&gnc-qof) { * }
 
-    method qof_session_events_pending(-->int64) is native(&gncmod-engine) { * }
-    method qof_session_process_events(-->int64) is native(&gncmod-engine) { * }
+    method qof_session_events_pending(-->int64) is native(&gnc-qof) { * }
+    method qof_session_process_events(-->int64) is native(&gnc-qof) { * }
 
-    method qof_session_export(QofSession $real_session, &percentage_func (Str $message, num64 $percent)) is native(&gncmod-engine) { * }
+    method qof_session_export(QofSession $real_session, &percentage_func (Str $message, num64 $percent)) is native(&gnc-qof) { * }
 
-    method qof_session_ensure_all_data_loaded() is native(&gncmod-engine) { * }
+    method qof_session_ensure_all_data_loaded() is native(&gnc-qof) { * }
 
-    method qof_session_end() is native(&gncmod-engine) { * }
+    method qof_session_end() is native(&gnc-qof) { * }
 
-    method qof_session_destroy() is native(&gncmod-engine) { * }
+    method qof_session_destroy() is native(&gnc-qof) { * }
 
     method DESTROY { self.qof_session_destroy() }
 }
